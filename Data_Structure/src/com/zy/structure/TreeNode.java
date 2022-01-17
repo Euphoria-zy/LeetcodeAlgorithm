17cd98de8ca426a76873535eedd9f6c4a77228db
Euphoria-zy <2978766226@qq.com>
2022-01-17 20:11:24 +0800

    2022/1/17

--- Data_Structure/src/com/zy/structure/TreeNode.java
+++ Data_Structure/src/com/zy/structure/TreeNode.java
@@ -5,11 +5,11 @@ public class TreeNode
     public int val;
     public TreeNode left;
     public TreeNode right;
-    TreeNode() {}
-    TreeNode(int value) {
+    public TreeNode() {}
+    public TreeNode(int value) {
         this.val = value;
     }
-    TreeNode(int val, TreeNode left, TreeNode right)
+    public TreeNode(int val, TreeNode left, TreeNode right)
     {
         this.val = val;
         this.left = left;
