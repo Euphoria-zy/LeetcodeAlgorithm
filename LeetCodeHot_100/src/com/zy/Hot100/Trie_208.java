a0b1b17cc66b727208636fd7021090138be8c6ba
Euphoria-zy <2978766226@qq.com>
2024-03-10 15:15:43 +0800

    2024/03/10 208. 实现 Trie (前缀树)

---
+++ LeetCodeHot_100/src/com/zy/Hot100/Trie_208.java
@@ -0,0 +1,66 @@
+package com.zy.Hot100;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+208. 实现 Trie (前缀树)
+Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。
+这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+Trie() 初始化前缀树对象。
+void insert(String word) 向前缀树中插入字符串 word 。
+boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+* */
+public class Trie_208 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Trie {
+
+    //单词都由26个小写字母组成
+    //每个节点包含26个子节点，以及isEnd字段，判断单词是否以该节点结尾
+    private Trie[] children;
+    private boolean isEnd;
+    public Trie() {
+        this.children = new Trie[26];
+        this.isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie trie = searchPrefix(word);
+        return trie != null && trie.isEnd;
+    }
+
+    public Trie searchPrefix(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null)
+                return null;
+            else
+                node = node.children[index];
+        }
+        return node;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie trie = searchPrefix(prefix);
+        return trie != null;
+    }
+}
